a4fe6232dd117c5eeeb7b5f78180f6cb6d9d6d7d
Brian Stiles <bstiles@bstiles.net>
2015-02-13 14:43:17 -0800

    Frame size tweaks.

--- old-init.el
+++ old-init.el
@@ -7,10 +7,10 @@
   (concat (getenv "HOME") "/" relative-path))
 
 (setq my-top 22)
-(setq my-left 250); 0
-(setq my-width-cols 174); 187 183 166
-(setq my-height-rows 52); 58 54 48
-(setq my-dock-height 0); 100
+(setq my-left 0); 0
+(setq my-width-cols 189); 174 183 166
+(setq my-height-rows 57); 52 54 48
+(setq my-dock-height 22); 100 0
 (setq my-dock-width 0); 30 0
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
